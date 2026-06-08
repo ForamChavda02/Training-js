d64ebc24ddc3f10a379308290378927ee526b52e
Foram Chavda <foramchavda52@gmail.com>
2026-06-08 18:56:10 +0530

    added a function and recursion

--- Task folder/Ticket Task/groupUserBydepartment.js
+++ Task folder/Ticket Task/groupUserBydepartment.js
@@ -117,29 +117,28 @@ children: []
 ]
 */
 //12:32 - 13:40
-function nestedArr(array) {
-    const result = [];
-    for (let i = 0; i < array.length; i++) {
-        array[i].children = [];
-        if (array[i].parentId === null) {
-            result.push(array[i]);
-        }
-        for (let j = 0; j < array.length; j++) {
-            if (array[j].parentId === array[i].id) {
-                array[i].children.push(array[j]);
-            }
+function getChildren(array, parentId) {
+    const children = [];
+    for(let i = 0; i < array.length; i++) {
+        if(array[i].parentId === parentId) {
+            array[i].children = getChildren(array, array[i].id);
+            children.push(array[i]);
         }
     }
-    return result;
+    return children;
 }
-console.log(JSON.stringify(nestedArr(([
-{ id: 1, parentId: null, name: "Electronics" },
-{ id: 2, parentId: 1, name: "Laptop" },
-{ id: 3, parentId: 1, name: "Mobile" },
-{ id: 4, parentId: 2, name: "Gaming Laptop" },
-{ id: 5, parentId: 2, name: "Business Laptop" },
-{ id: 6, parentId: 3, name: "Android" }])), null, 4));
-
+function nestedArr(array) {
+    return getChildren(array, null);
+}
+const data = [
+    { id: 1, parentId: null, name: "Electronics" },
+    { id: 2, parentId: 1, name: "Laptop" }, 
+    { id: 3, parentId: 1, name: "Mobile" },
+    { id: 4, parentId: 2, name: "Gaming Laptop" },
+    { id: 5, parentId: 2, name: "Business Laptop" },
+    { id: 6, parentId: 3, name: "Android" }
+];
+console.log(JSON.stringify(nestedArr(data), null, 4));
 /*
 [
     {
@@ -182,5 +181,5 @@ console.log(JSON.stringify(nestedArr(([
         ]
     }
 ]
-
+    
 */
